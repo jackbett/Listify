99a56b5b0f1a3aeaa768727d9c4072782b0066f5
Jack B <jackabett@gmail.com>
2023-11-12 12:48:05 -0500

    Added in home screen fixes for spacing, ellipse and icon if not existing

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -13,51 +13,77 @@ const HomeScreen = () => {
   const screenHeight = Dimensions.get('window').height;
   const topPadding = screenHeight * 0.08;
 
-  return (
-    <View style={{ flex: 1, paddingTop: topPadding, backgroundColor: "#151515"}}>
-  {/* Settings icon */}
-      <View style={{ flexDirection: 'row', padding: 10, alignItems: 'center' }}>
-          {/* First view, the size of the icon */}
-          <View>
-            <TouchableWithoutFeedback >
-              <View>
-                <Ionicons name={"settings"} size={24} color="white" />
-              </View>
-            </TouchableWithoutFeedback>
-          </View>
+  const InitialIcon = ({ initials }) => {
+    return (
+      <View
+        style={{
+          // backgroundColor: 'blue',
+          alignItems: 'center',
+          justifyContent: 'center',
+          // borderRadius: 50,
+          width: 50,
+          height: 50,
+        }}>
+        <Text style={{ color: 'white', fontSize: 50 }}>{initials}</Text>
+      </View>
+    );
+  };
 
-          {/* Second view, goes all the way across */}
-          <View style={{ flex: 1 }}>
-            {/* Your content for the second view goes here */}
-          </View>
+  return (
+    <View style={{ flex: 1, paddingTop: topPadding, backgroundColor: "#151515" }}>
+      {/* Settings icon */}
+      <View style={{ flexDirection: 'row', padding: 10, alignItems: 'center' }}>
+        {/* First view, the size of the icon */}
+        <View>
+          <TouchableWithoutFeedback >
+            <View>
+              <Ionicons name={"settings"} size={24} color="white" />
+            </View>
+          </TouchableWithoutFeedback>
+        </View>
+
+        {/* Second view, goes all the way across */}
+        <View style={{ flex: 1 }}>
+          {/* Your content for the second view goes here */}
+        </View>
       </View>
 
       {/* Top section */}
-      <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center", padding: 10, height: "15%", backgroundColor: "#151515" }}>
-        <View>
-          <Text style={{ color: "white", fontSize: 24, fontFamily: "AvenirNext-Bold" }}>
-            Hello, {userProfile?.display_name}
-          </Text>
-          {/* Additional text */}
-          <Text style={{ color: "white", fontSize: 16, fontFamily: "AvenirNext-Medium" }}>
-            @{userProfile?.id}
-          </Text>
-        </View>
-        {userProfile && userProfile.images && userProfile.images.length > 0 ? (
-          <Image
-            source={{ uri: userProfile.images[0].url }}
-            style={{ width: 50, height: 50, borderRadius: 50 }}
-          />
-        ) : (
-          <View style={{ backgroundColor: "grey", borderRadius: 50, padding: 10}}>
-            <Ionicons name="person-outline" size={50} color="black" />
+      <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center", padding: 0, height: "12.5%", backgroundColor: "#151515" }}>
+      {/* Left section for user display name */}
+      <View style={{ flexDirection: 'row', alignItems: 'center', padding: 10 }}>
+        {/* Left section for user display name */}
+        <View style={{ flex: 1, alignItems: 'flex-start', maxWidth: '80%', padding: 5 }}>
+          <View>
+            <Text numberOfLines={1} ellipsizeMode="tail" style={{ color: "white", fontSize: 24, fontFamily: "AvenirNext-Bold" }}>
+              Hello {userProfile?.display_name}!
+            </Text>
+            {/* Additional text */}
+            <Text numberOfLines={1} ellipsizeMode="tail" style={{ color: "white", fontSize: 16, fontFamily: "AvenirNext-Medium" }}>
+              @{userProfile?.id}
+            </Text>
           </View>
-        )}
+        </View>
+
+        {/* Right section for profile image */}
+        <View style={{  backgroundColor: "#1ED760",  borderRadius: 50, padding: 10,}}>
+          {userProfile && userProfile.images && userProfile.images.length > 0 ? (
+            <Image
+              source={{ uri: userProfile.images[0].url }}
+              style={{ }}
+            />
+          ) : (           
+            <InitialIcon initials={userProfile?.display_name ? userProfile.display_name[0].toUpperCase() 
+              : <Ionicons name="person-outline" size={50} color="white" />} />
+            //<Ionicons name="person-outline" size={50} color="black" />
+          )}
+        </View>
       </View>
+    </View>
 
       {/* Additional view in the top section */}
       <View style={{ alignItems: "center", padding: 10, backgroundColor: "#151515" }}>
-        <Text style={{ color: "white", fontSize: 20, fontFamily: "AvenirNext-Bold" }}>
+        <Text numberOfLines={1} ellipsizeMode="tail" style={{ color: "white", fontSize: 20, fontFamily: "AvenirNext-Bold" }}>
           A song you like with a color in the title
         </Text>
       </View>
