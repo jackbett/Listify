4644d855e9d8c649547163dc1a89ef78ec4945a5
Jack B <jackabett@gmail.com>
2023-11-11 20:41:15 -0500

    Fix for expired bearer token, logic check to see if exists, and if still valid, else go to log in screen

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,69 +1,91 @@
-// AppNavigation.js
-
-import React, { useContext, useState, useEffect, useRef  } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { AuthContext } from "../contexts/AuthContext";
 import HomeNavigation from "./HomeNavigator";
 import LoginNavigation from "./LoginNavigator";
-import LoadingScreen from "../screens/LoadingScreen"; // Create a LoadingScreen component
+import LoadingScreen from "../screens/LoadingScreen";
 
 const Stack = createNativeStackNavigator();
 
-function AppNavigation() {
+const API_URL = 'https://api.spotify.com/v1/me';
+const LOADING_TIMEOUT = 500;
+
+const validateToken = async (accessToken) => {
+  try {
+    const response = await fetch(API_URL, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    return response.ok;
+  } catch (error) {
+    console.error('Error validating access token:', error);
+    return false;
+  }
+};
+
+const AppNavigation = () => {
   const { state } = useContext(AuthContext);
   const [loading, setLoading] = useState(true);
-  const isUnmounted = useRef(false);
+  const [initialRoute, setInitialRoute] = useState("Loading");
+  const [isTokenValidated, setIsTokenValidated] = useState(false);
 
   useEffect(() => {
-    // Set the ref to false when the component mounts
-    isUnmounted.current = false;
+    const checkTokenValidity = async () => {
+      if (state.accessToken) {
+        const isValid = await validateToken(state.accessToken);
 
-    // If authentication state is loaded, wait for at least 5 seconds and then set loading to false
-    const timeout = setTimeout(() => {
-      if (!isUnmounted.current) {
-        setLoading(false);
+        if (isValid) {
+          setInitialRoute("Home");
+        } else {
+          setInitialRoute("Login");
+        }
+      } else {
+        setInitialRoute("Login");
       }
-    }, 1000);
 
-    // Clear the timeout if the component unmounts
-    return () => {
-      isUnmounted.current = true;
-      clearTimeout(timeout);
+      setIsTokenValidated(true);
     };
+
+    const timeout = setTimeout(() => {
+      checkTokenValidity();
+      setLoading(false);
+    }, LOADING_TIMEOUT);
+
+    return () => clearTimeout(timeout);
   }, [state.accessToken]);
 
-  if (loading) {
-    return <LoadingScreen />; // Display a loading screen while authentication state is loading
+  useEffect(() => {
+    // Reset the navigation state when the initialRoute changes
+    if (isTokenValidated) {
+      setInitialRoute("Loading");
+    }
+  }, [initialRoute, isTokenValidated]);
 
+  if (loading) {
+    return <LoadingScreen />;
   }
 
   return (
-  
-      <Stack.Navigator
-        initialRouteName={state.accessToken ? "Home" : "Login"}
-        screenOptions={{
-          animationEnabled: false, // Disable animation between screens
+    <Stack.Navigator initialRouteName={initialRoute}>
+      <Stack.Screen
+        name="Login"
+        component={LoginNavigation}
+        options={{
+          title: "Listify - Login",
+          headerShown: false,
         }}
-      >
-        <Stack.Screen
-          name="Login"
-          component={LoginNavigation}
-          options={{
-            title: "Listify - Login",
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={HomeNavigation}
-          options={{
-            title: "Listify - Home",
-            headerShown: false,
-          }}
-        />
-      </Stack.Navigator>
-  
+      />
+      <Stack.Screen
+        name="Home"
+        component={HomeNavigation}
+        options={{
+          title: "Listify - Home",
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
   );
-}
+};
 
 export default AppNavigation;
