8be7e790d4b2ac9f3445442498cec9bf20da479c
Jack B <jackabett@gmail.com>
2023-11-11 23:36:53 -0500

    Fixed auth context for login and refreshing valid token if expired to redirect to home

--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -2,25 +2,39 @@
 
 import React, { createContext, useState, useEffect } from "react";
 import * as SecureStore from 'expo-secure-store';
+import { encode } from 'base-64';
 
 const AuthContext = createContext();
 
+const AUTHORIZATION_ENDPOINT = "https://accounts.spotify.com/authorize";
+const TOKEN_ENDPOINT = "https://accounts.spotify.com/api/token";
+const CLIENT_ID = "c7aa783dc6ec4440886b382a9fd31e79";
+const SECRET_ID = "9773862fc3794b5eab58e94953f81e9d";
+const CODE_RESPONSE_TYPE = "code";
+const REDIRECT_URI = "exp://jp-f6y.jackabett.listify.exp.direct:80/--/spotify-auth-callback";
+const SCOPE = ["user-read-email", "playlist-modify-public"];
+const AUTH_CODE_GRANT_TYPE = "authorization_code";
+const CONTENT_TYPE_HEADER = "application/x-www-form-urlencoded";
+const REFRESH_GRANT_TYPE = "refresh_token";
+const VALIDATE_ENDPOINT = "https://api.spotify.com/v1/me";
+
 const initialState = {
-  authorizationEndpoint: "https://accounts.spotify.com/authorize",
-  tokenEndpoint: "https://accounts.spotify.com/api/token",
-  clientId: "c7aa783dc6ec4440886b382a9fd31e79",
-  secretId: "9773862fc3794b5eab58e94953f81e9d",
-  codeResponseType: "code",
-  redirectUri: "exp://jp-f6y.jackabett.listify.exp.direct:80/--/spotify-auth-callback",
-  scope: ["user-read-email", "playlist-modify-public"],
-  authCodeGrantType: "authorization_code",
-  contentTypeHeader: "application/x-www-form-urlencoded",
+  authorizationEndpoint: AUTHORIZATION_ENDPOINT,
+  tokenEndpoint: TOKEN_ENDPOINT,
+  clientId: CLIENT_ID,
+  secretId: SECRET_ID,
+  codeResponseType: CODE_RESPONSE_TYPE,
+  redirectUri: REDIRECT_URI,
+  scope: SCOPE,
+  authCodeGrantType: AUTH_CODE_GRANT_TYPE,
+  contentTypeHeader: CONTENT_TYPE_HEADER,
   returnedCode: "",
   accessToken: "",
   tokenType: "",
   tokenExpiresIn: "",
   refreshToken: "",
-  refreshGrantType: "refresh_token",
+  refreshGrantType: REFRESH_GRANT_TYPE,
+  validationUrl: VALIDATE_ENDPOINT,
 };
 
 function AuthProvider({ children }) {
@@ -65,11 +79,94 @@ function AuthProvider({ children }) {
     }
   };
 
+  const validateToken = async () => {
+    try {
+      console.log(VALIDATE_ENDPOINT);
+
+      const response = await fetch(VALIDATE_ENDPOINT, {
+        headers: {
+          Authorization: `Bearer ${state.accessToken}`,
+        },
+      });
+      return response.ok; 
+    } catch (error) {
+      console.error('Error validating access token:', error);
+      return false;
+    }
+  };
+
+  const refreshToken = async () => {
+    try {
+      const bodyParams = new URLSearchParams({
+        grant_type: REFRESH_GRANT_TYPE,
+        refresh_token: state.refreshToken,
+      });
+
+      const response = await fetch(TOKEN_ENDPOINT, {
+        method: 'POST',
+        headers: {
+          'Content-Type': CONTENT_TYPE_HEADER,
+          Authorization: `Basic ${encode(`${CLIENT_ID}:${SECRET_ID}`)}`,
+        },
+        body: bodyParams.toString(),
+      });
+
+      const responseData = await response.json();
+      console.log('Token refresh response:', response.status, responseData);
+
+      if (!response.ok) {
+        throw new Error('Token refresh failed');
+      }
+
+      const updatedState = {
+        ...state,
+        accessToken: responseData.access_token,
+        tokenType: responseData.token_type,
+        tokenExpiresIn: responseData.expires_in,
+        refreshToken: responseData.refresh_token || state.refreshToken,
+      };
+
+      await SecureStore.setItemAsync('authState', JSON.stringify(updatedState));
+      setState(updatedState);
+    } catch (error) {
+      console.error('Error refreshing access token:', error);
+      throw error;
+    }
+  };
+
+  const signOut = async () => {
+    try {
+      // Revoke the Spotify refresh token
+      await fetch(TOKEN_ENDPOINT, {
+        method: 'POST',
+        headers: {
+          'Content-Type': CONTENT_TYPE_HEADER,
+          Authorization: `Basic ${encode(`${CLIENT_ID}:${SECRET_ID}`)}`,
+        },
+        body: new URLSearchParams({
+          token: state.refreshToken,
+          token_type_hint: 'refresh_token',
+        }),
+      });
+
+      // Clear the authentication state and remove tokens from storage
+      const updatedState = { ...initialState };
+      await SecureStore.deleteItemAsync('authState');
+      setState(updatedState);
+    } catch (error) {
+      console.error('Error signing out:', error);
+      throw error;
+    }
+  };
+
   return (
     <AuthContext.Provider
       value={{
         state,
         setAuthStates,
+        validateToken,
+        refreshToken,
+        signOut,
       }}
     >
       {children}
--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -6,61 +6,47 @@ import LoginNavigation from "./LoginNavigator";
 import LoadingScreen from "../screens/LoadingScreen";
 
 const Stack = createNativeStackNavigator();
-
-const API_URL = 'https://api.spotify.com/v1/me';
 const LOADING_TIMEOUT = 500;
 
-const validateToken = async (accessToken) => {
-  try {
-    const response = await fetch(API_URL, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-    return response.ok;
-  } catch (error) {
-    console.error('Error validating access token:', error);
-    return false;
-  }
-};
-
 const AppNavigation = () => {
-  const { state } = useContext(AuthContext);
+  const { state, validateToken, refreshToken } = useContext(AuthContext);
   const [loading, setLoading] = useState(true);
   const [initialRoute, setInitialRoute] = useState("Loading");
-  const [isTokenValidated, setIsTokenValidated] = useState(false);
 
   useEffect(() => {
     const checkTokenValidity = async () => {
-      if (state.accessToken) {
-        const isValid = await validateToken(state.accessToken);
+      try {
+        if (state.accessToken) {
+          console.log('accessTokenExists');
+          const isValid = await validateToken();
 
-        if (isValid) {
-          setInitialRoute("Home");
+          if (isValid) {
+            console.log('accessTokenValid');
+            setInitialRoute("Home");
+          } else {
+            // Attempt to refresh the token
+            console.log('tryingToRefreshToken');
+            await refreshToken();
+            console.log('tokenRefreshed');
+            setInitialRoute("Home");
+          }
         } else {
-          setInitialRoute("Login");
+          console.log('noAccessToken');
         }
-      } else {
+      } catch (error) {
+        console.error('Error checking token validity:', error);
         setInitialRoute("Login");
+      } finally {
+        setLoading(false);
       }
-
-      setIsTokenValidated(true);
     };
 
     const timeout = setTimeout(() => {
       checkTokenValidity();
-      setLoading(false);
     }, LOADING_TIMEOUT);
 
     return () => clearTimeout(timeout);
-  }, [state.accessToken]);
-
-  useEffect(() => {
-    // Reset the navigation state when the initialRoute changes
-    if (isTokenValidated) {
-      setInitialRoute("Loading");
-    }
-  }, [initialRoute, isTokenValidated]);
+  }, [state.accessToken, validateToken, refreshToken]);
 
   if (loading) {
     return <LoadingScreen />;
