d62f2e1a71164c9fe3efb734d1d9e054a92f3459
Jack B <jackabett@gmail.com>
2023-11-12 15:02:45 -0500

    Updated currently playing

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,39 +1,46 @@
-import React, { useContext, useState, useEffect } from "react";
-import { Text, View, Image, Dimensions, TouchableWithoutFeedback } from "react-native";
+import React, { useContext, useEffect } from "react";
+import { Text, View, Image, Dimensions, TouchableOpacity } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { Ionicons } from "@expo/vector-icons";
-import SpotifyApi from "../api/SpotifyApi"; // Update the path accordingly
-import { AuthContext } from "../api/AuthService"; // Add this import
+import SpotifyApi from "../api/SpotifyApi";
+import { AuthContext } from "../api/AuthService";
+import { useUserProfile, useCurrentlyPlaying } from "../api/SpotifyApi";
+import Marquee from "react-native-marquee"; // Import Marquee
 
 const HomeScreen = () => {
   const { state } = useContext(AuthContext);
-  const { userProfile } = SpotifyApi({ accessToken: state.accessToken });
+  const accessToken = state.accessToken;
+
+  const userProfile = useUserProfile(accessToken);
+  const currentlyPlaying = useCurrentlyPlaying(accessToken);
+
+  // console.log(currentlyPlaying)
 
   // Calculate padding based on screen height
-  const screenHeight = Dimensions.get('window').height;
+  const screenHeight = Dimensions.get("window").height;
   const topPadding = screenHeight * 0.08;
 
-  const InitialIcon = ({ initials }) => {
-    return (
-      <View
-        style={{
-        }}>
-        <Text style={{ color: 'white', fontSize: 50 }}>{initials}</Text>
-      </View>
-    );
-  };
+  const InitialIcon = ({ initials }) => (
+    <View>
+      <Text style={{ color: "white", fontSize: 50 }}>{initials}</Text>
+    </View>
+  );
+
+  useEffect(() => {
+    // Additional logic or side effects can be added here
+  }, []);
 
   return (
     <View style={{ flex: 1, paddingTop: topPadding, backgroundColor: "#151515" }}>
       {/* Settings icon */}
-      <View style={{ flexDirection: 'row', padding: 10, alignItems: 'center' }}>
+      <View style={{ flexDirection: "row", paddingHorizontal: 15, paddingVertical: 10, alignItems: "center" }}>
         {/* First view, the size of the icon */}
         <View>
-          <TouchableWithoutFeedback >
+          <TouchableOpacity>
             <View>
               <Ionicons name={"settings"} size={24} color="white" />
             </View>
-          </TouchableWithoutFeedback>
+          </TouchableOpacity>
         </View>
 
         {/* Second view, goes all the way across */}
@@ -44,39 +51,44 @@ const HomeScreen = () => {
 
       {/* Top section */}
       <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center", padding: 0, height: "12.5%", backgroundColor: "#151515" }}>
-      {/* Left section for user display name */}
-      <View style={{ flexDirection: 'row', alignItems: 'center', padding: 10 }}>
         {/* Left section for user display name */}
-        <View style={{ flex: 1, alignItems: 'flex-start', maxWidth: '80%', padding: 5 }}>
-          <View>
-            <Text numberOfLines={1} ellipsizeMode="tail" style={{ color: "white", fontSize: 24, fontFamily: "AvenirNext-Bold" }}>
-              Hello {userProfile?.display_name}!
-            </Text>
-            {/* Additional text */}
-            <Text numberOfLines={1} ellipsizeMode="tail" style={{ color: "white", fontSize: 16, fontFamily: "AvenirNext-Medium" }}>
-              @{userProfile?.id}
-            </Text>
+        <View style={{ flexDirection: "row", alignItems: "center", padding: 10 }}>
+          {/* Left section for user display name */}
+          <View style={{ flex: 1, alignItems: "flex-start", maxWidth: "80%", padding: 5 }}>
+            <View>
+              <Text numberOfLines={1} ellipsizeMode="tail" style={{ color: "white", fontSize: 24, fontFamily: "AvenirNext-Bold" }}>
+                Hello {userProfile?.display_name}!
+              </Text>
+              {/* Additional text */}
+              <Text numberOfLines={1} ellipsizeMode="tail" style={{ color: "white", fontSize: 16, fontFamily: "AvenirNext-Medium" }}>
+                @{userProfile?.id}
+              </Text>
+            </View>
+          </View>
+
+          {/* Right section for profile image */}
+          <View
+            style={{
+              backgroundColor: "#1ED760",
+              borderRadius: 50,
+              alignItems: "center",
+              justifyContent: "center",
+              overflow: "hidden",
+              width: 75,
+              height: 75,
+            }}>
+            {userProfile && userProfile.images && userProfile.images.length > 0 ? (
+              <Image
+                source={{ uri: userProfile.images[0].url }}
+                style={{ width: "100%", height: "100%", borderRadius: 50 }}
+                resizeMode="cover"
+              />
+            ) : (
+              <InitialIcon initials={userProfile?.display_name ? userProfile.display_name[0].toUpperCase() : <Ionicons name="person-outline" size={50} color="white" />} />
+            )}
           </View>
         </View>
-
-        {/* Right section for profile image */}
-        <View style={{  backgroundColor: "#1ED760",  borderRadius: 50, 
-        alignItems: 'center', justifyContent: 'center', overflow: "hidden", width: 75,
-        height: 75,
-      }}>
-          {userProfile && userProfile.images && userProfile.images.length > 0 ? (
-            <Image
-              source={{ uri: userProfile.images[0].url }}
-              style={{ width: '100%', height: '100%', borderRadius: 50 }}
-              resizeMode="cover"
-              />
-          ) : (           
-            <InitialIcon initials={userProfile?.display_name ? userProfile.display_name[0].toUpperCase() 
-              : <Ionicons name="person-outline" size={50} color="white" />} />
-          )}
-        </View>
       </View>
-    </View>
 
       {/* Additional view in the top section */}
       <View style={{ alignItems: "center", padding: 10, backgroundColor: "#151515" }}>
@@ -85,13 +97,46 @@ const HomeScreen = () => {
         </Text>
       </View>
 
-      <LinearGradient
-        colors={["#040306", "#131624"]}
-        style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-      >
-        <Text style={{ color: "white" }}>Hello, {userProfile?.id}</Text>
+      <LinearGradient colors={["#040306", "#131624"]} style={{ flex: 1, alignItems: "flex-start", justifyContent: "flex-start" }}>
+        {/* Display currently playing track details */}
+        {currentlyPlaying && currentlyPlaying.item ? (
+          <View style={{ padding: 10, width: "50%" }}>
+            {/* Currently Playing Text */}
+            <Text style={{ color: "white", fontSize: 20, fontFamily: "AvenirNext-Bold", marginBottom: 5 }}>
+              Currently Playing
+            </Text>
 
-        <Text style={{ color: "white" }}>Hello, {userProfile?.display_name}</Text>
+            {/* Nested View for Album Image and Song Details */}
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              {/* Album Image on the left side */}
+              <Image source={{ uri: currentlyPlaying.item.album.images[0].url }} style={{ width: 60, height: 60, borderRadius: 10, marginRight: 10 }} />
+
+              {/* Song details on the right side */}
+              <View>
+                {/* Song Name with scrolling effect */}
+                <Marquee style={{ color: "white", fontSize: 18, fontFamily: "AvenirNext-Bold", marginBottom: 5 }}
+                speed={.15}
+                delay={2000}
+                >
+                {currentlyPlaying.item.name}
+              </Marquee>
+
+                {/* Artist Name */}
+                <Text numberOfLines={2} ellipsizeMode="tail" style={{ color: "white", fontSize: 16, fontFamily: "AvenirNext", marginBottom: 5 }}>
+                  {currentlyPlaying.item.artists.map((artist) => artist.name).join(", ")}
+                </Text>
+
+                {/* <MarqueeView duration={3000} loop style={{ color: "white", fontSize: 16, fontFamily: "AvenirNext-Medium" }}>
+                  {currentlyPlaying.item.artists.map((artist) => artist.name).join(", ")}
+                </MarqueeView> */}
+              </View>
+            </View>
+          </View>
+        ) : (
+          <Text style={{ color: "white" }}>No track is currently playing</Text>
+        )}
+
+        {/* Additional content or styles can be added as needed */}
       </LinearGradient>
     </View>
   );
